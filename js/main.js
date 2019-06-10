75ddd72a3e8c02f82a3b5f9f2a81f5b2240370d8
Camille Jacques <camille.jacques75@gmail.com>
2019-06-10 14:18:29 +0200

    cleaned key listener

--- js/main.js
+++ js/main.js
@@ -7,22 +7,29 @@
     const allowedKeys = ["ArrowUp", "ArrowRight", "ArrowDown", "ArrowLeft"];
     const inputArray = [];
     window.addEventListener("keydown", function newInput(evt) {
+      // Creates the array according to the pressed keys. It can be no longer than the instructions array.
       if (allowedKeys.includes(evt.key) && inputArray.length < instructions.length) {
         let direction = evt.key.split("Arrow").pop().toLocaleLowerCase();
         inputArray.push(`${direction}`);
-      };
-      console.log(inputArray);
-      // if (inputArray.length = instructions.length) {
-      //   for (let i = 0; i < instructions.length; i++) {
-      //     if (inputArray[i] = instructions[i])
-      //   }
-      //   console.log("You won");
+        if (inputArray.length === instructions.length) {
+          console.log(inputArray);
+          compareArrays(inputArray);
+        }
       }
-    });  
+    });
   };
 
-  // function compareArrays() {
-  // }
+  function compareArrays(inputArray) {
+    if (inputArray.length = instructions.length) {
+      for (let i = 0; i < instructions.length; i++) {
+        if (inputArray[i] != instructions[i]) {
+          console.log("You lost :(");
+        } else {
+          console.log("You won!");
+        }
+      };
+    }
+  }
 
   window.addEventListener("DOMContentLoaded",listenKeys);
 }());
