a7ee885225fc6daf4c2a6c4578630c2f2c901da6
Camille Jacques <camille.jacques75@gmail.com>
2019-06-14 11:05:47 +0200

    updated links for github pages

--- js/scorebar-updaters.js
+++ js/scorebar-updaters.js
@@ -1,21 +1,21 @@
 function updateScoreOne() {
   let scorebar = document.querySelector("#player1-score");
   if (newGame.playerScore == 3) {
-    scorebar.innerHTML = `<img src="/img/sprite_sheetclefairysays_ballclosed.png" alt="">
-    <img src="/img/sprite_sheetclefairysays_ballclosed.png" alt="">
-    <img src="/img/sprite_sheetclefairysays_ballclosed.png" alt="">`;
+    scorebar.innerHTML = `<img src="./img/sprite_sheetclefairysays_ballclosed.png" alt="">
+    <img src="./img/sprite_sheetclefairysays_ballclosed.png" alt="">
+    <img src="./img/sprite_sheetclefairysays_ballclosed.png" alt="">`;
   } else if (newGame.playerScore == 2) {
-    scorebar.innerHTML = `<img src="/img/sprite_sheetclefairysays_ballopen.png" alt="">
-    <img src="/img/sprite_sheetclefairysays_ballclosed.png" alt="">
-    <img src="/img/sprite_sheetclefairysays_ballclosed.png" alt="">`;
+    scorebar.innerHTML = `<img src="./img/sprite_sheetclefairysays_ballopen.png" alt="">
+    <img src="./img/sprite_sheetclefairysays_ballclosed.png" alt="">
+    <img src="./img/sprite_sheetclefairysays_ballclosed.png" alt="">`;
   } else if (newGame.playerScore == 1) {
-    scorebar.innerHTML = `<img src="/img/sprite_sheetclefairysays_ballopen.png" alt="">
-    <img src="/img/sprite_sheetclefairysays_ballopen.png" alt="">
-    <img src="/img/sprite_sheetclefairysays_ballclosed.png" alt="">`;
+    scorebar.innerHTML = `<img src="./img/sprite_sheetclefairysays_ballopen.png" alt="">
+    <img src="./img/sprite_sheetclefairysays_ballopen.png" alt="">
+    <img src="./img/sprite_sheetclefairysays_ballclosed.png" alt="">`;
   } else if (newGame.playerScore == 0) {
-    scorebar.innerHTML = `<img src="/img/sprite_sheetclefairysays_ballopen.png" alt="">
-    <img src="/img/sprite_sheetclefairysays_ballopen.png" alt="">
-    <img src="/img/sprite_sheetclefairysays_ballopen.png" alt="">`;
+    scorebar.innerHTML = `<img src="./img/sprite_sheetclefairysays_ballopen.png" alt="">
+    <img src="./img/sprite_sheetclefairysays_ballopen.png" alt="">
+    <img src="./img/sprite_sheetclefairysays_ballopen.png" alt="">`;
   };
   turnResponseCount++;
   checkTurnResponseCount()
@@ -24,21 +24,21 @@ function updateScoreOne() {
 function updateScoreTwo() {
   let scorebar = document.querySelector("#player2-score");
   if (newGame2.playerScore == 3) {
-    scorebar.innerHTML = `<img src="/img/sprite_sheetclefairysays_ballclosed.png" alt="">
-    <img src="/img/sprite_sheetclefairysays_ballclosed.png" alt="">
-    <img src="/img/sprite_sheetclefairysays_ballclosed.png" alt="">`;
+    scorebar.innerHTML = `<img src="./img/sprite_sheetclefairysays_ballclosed.png" alt="">
+    <img src="./img/sprite_sheetclefairysays_ballclosed.png" alt="">
+    <img src="./img/sprite_sheetclefairysays_ballclosed.png" alt="">`;
   } else if (newGame2.playerScore == 2) {
-    scorebar.innerHTML = `<img src="/img/sprite_sheetclefairysays_ballopen.png" alt="">
-    <img src="/img/sprite_sheetclefairysays_ballclosed.png" alt="">
-    <img src="/img/sprite_sheetclefairysays_ballclosed.png" alt="">`;
+    scorebar.innerHTML = `<img src="./img/sprite_sheetclefairysays_ballopen.png" alt="">
+    <img src="./img/sprite_sheetclefairysays_ballclosed.png" alt="">
+    <img src="./img/sprite_sheetclefairysays_ballclosed.png" alt="">`;
   } else if (newGame2.playerScore == 1) {
-    scorebar.innerHTML = `<img src="/img/sprite_sheetclefairysays_ballopen.png" alt="">
-    <img src="/img/sprite_sheetclefairysays_ballopen.png" alt="">
-    <img src="/img/sprite_sheetclefairysays_ballclosed.png" alt="">`;
+    scorebar.innerHTML = `<img src="./img/sprite_sheetclefairysays_ballopen.png" alt="">
+    <img src="./img/sprite_sheetclefairysays_ballopen.png" alt="">
+    <img src="./img/sprite_sheetclefairysays_ballclosed.png" alt="">`;
   } else if (newGame2.playerScore == 0) {
-    scorebar.innerHTML = `<img src="/img/sprite_sheetclefairysays_ballopen.png" alt="">
-    <img src="/img/sprite_sheetclefairysays_ballopen.png" alt="">
-    <img src="/img/sprite_sheetclefairysays_ballopen.png" alt="">`;
+    scorebar.innerHTML = `<img src="./img/sprite_sheetclefairysays_ballopen.png" alt="">
+    <img src="./img/sprite_sheetclefairysays_ballopen.png" alt="">
+    <img src="./img/sprite_sheetclefairysays_ballopen.png" alt="">`;
   };
   turnResponseCount++;
   checkTurnResponseCount()
